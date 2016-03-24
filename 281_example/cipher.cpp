2119b4bdc8014dff7004f17e8bfe8898c3a655ca
Ian Pudney <ian.pudney.git@gmail.com>
2016-03-24 14:59:33 -0400

    adding 281 example code, with a bugfix because it was wrong

---
+++ 281_example/cipher.cpp
@@ -0,0 +1,47 @@
+// Jordan Ridenour - Homework 1
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void breakCipher(char *str) {
+	const int SIZE = strlen(str);
+	char *end = str;
+	char temp;
+	char* t_end;
+
+	// Reverse individual words
+	for (int i = 0; i < SIZE; ++i) {
+		if (*(end + 1) == ' ' || *(end + 1) == '\0') {
+			t_end = end;
+
+			while (str < end) {
+				temp = *str;
+				*str = *end;
+				*end = temp;
+				str++; end--;
+			}
+
+			end = t_end + 1;
+			str = end + 1;
+			i++;
+		}
+
+		end++;
+	}
+
+	// Reset start and end pointers
+	str = end - SIZE - 2;
+	end = str + SIZE + 1;
+
+	// Reverse in order of letters
+	str++; end--; //this line added by Ian Pudney, because provided submission has a segfault
+	while (str < end) {
+
+		temp = *str;
+		*str = *end;
+		*end = temp;
+		str++; end--;
+	}
+}
---
+++ 281_example/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+void breakCipher(char *str);
+
+void debug(string str) {
+	char* strToTest = strdup(str.c_str());
+	breakCipher(strToTest);
+	cout << "encrypted: " << strToTest << endl;
+	breakCipher(strToTest);
+	cout << "decrypted: " << strToTest << endl;
+	free(strToTest);
+}
+
+int main() {
+	debug("Hi! My name is Jordan Ridenour.");
+	
+}
