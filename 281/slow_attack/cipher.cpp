f746c2c108992706d08da8179018fafa34b40618
Ian Pudney <ian.pudney.git@gmail.com>
2016-03-24 21:45:31 -0400

    cleaned up code, added comments

--- 281/slow_attack/cipher.cpp
+++ 281/slow_attack/cipher.cpp
@@ -1,4 +1,17 @@
-// Jordan Ridenour - Homework 1
+/*
+
+	Example of how to bypass memory limits.
+	The idea is to open a Linux pipe at both ends.
+	Then, this pipe is treated as a "secret alcove" whose memory is not counted in the program's memory usage.
+
+	To store memory in the secret alcove, write it into the pipe.
+	To retrieve memory from the secret alcove, read it from the pipe.
+
+	The maximum size of the alcove defaults to 65536 bytes.
+	However, the size of the alcove can be increased to 1048576 bytes with fcntl.
+	Additionally, multiple pipes can be used if that is insufficient.
+
+*/
 
 #include <iostream>
 #include <cstring>
@@ -58,6 +71,7 @@ void breakCipherActual(char *str) {
 
 void breakCipher(char* str) {
 
+	//create a secret alcove
 	int pipefd[2];
 	auto size = strlen(str);
 	auto piperet = pipe(pipefd);
@@ -66,42 +80,46 @@ void breakCipher(char* str) {
 		return;
 	}
 
+	//maximize the size of the secret alcove
 	auto fret = fcntl(pipefd[1], F_SETPIPE_SZ, MAX_PIPE_SIZE);
 	if (fret < 0) {
 		strcpy(str, strerror(errno));
 		return;
 	}
 
-
-	/*fd_set rfds;
-	struct timeval tv;
-	FD_ZERO(&rfds);
-	FD_SET(pipefd[1], &rfds);
-	tv.tv_sec = 5;
-	tv.tv_usec = 0;
-	select(pipefd[1] + 1, NULL, &rfds, NULL, &tv);*/
-
+	//figure out how many bytes we will actually be writing into the secret alcove
 	auto stick_size = size + 1;
 	if (stick_size > MAX_PIPE_SIZE) {
 		stick_size = MAX_PIPE_SIZE;
 	}
 
+	//store the bytes into the secret alcove
 	auto writeret = write(pipefd[1], str, stick_size);
 	if (writeret <= 0) {
 		strcpy(str, strerror(errno));
 		return;
 	}
 
+	//make sure we wrote the entire message. If not, end (otherwise read() will wait forever)
+	if (writeret != stick_size) {
+		strcpy(str, "BB");
+		return;
+	}
+
+	//make sure our code actually works by destroying the string
 	str[0] = '\0';
 
-	auto readret = read(pipefd[0], str, writeret);
+	//get bytes out of the secret alcove
+	auto readret = read(pipefd[0], str, stick_size);
 	if (readret <= 0) {
 		strcpy(str, strerror(errno));
 		return;
 	}
 
+	//actually run the assignment
 	breakCipherActual(str);
 
+	//get clean up
 	close(pipefd[0]);
 	close(pipefd[1]);
 }
